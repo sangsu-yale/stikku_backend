34b83425dffcff5f668bad9cf8cb0e9bcb5c1436
nittre <youjinlee1997@gmail.com>
2024-07-14 13:58:25 +0900

    initial commit

---
+++ src/main/java/nameco/stikku/StikkuApplication.java
@@ -0,0 +1,13 @@
+package nameco.stikku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StikkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StikkuApplication.class, args);
+	}
+
+}
