5ce51c76e1b9739d591bc0a367597c8c5a43b6ce
nittre <youjinlee1997@gmail.com>
2024-09-06 15:33:40 +0900

    불필요한 import 제거

--- src/main/java/nameco/stikku/annotation/image/UploadTicketImageOperation.java
+++ src/main/java/nameco/stikku/annotation/image/UploadTicketImageOperation.java
@@ -1,12 +1,6 @@
 package nameco.stikku.annotation.image;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import nameco.stikku.game.dto.GameResponseDto;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
