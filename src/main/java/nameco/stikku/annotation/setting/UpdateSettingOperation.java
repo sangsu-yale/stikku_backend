96619b1a29db649cdb23a0d37240644420efe71e
nittre <youjinlee1997@gmail.com>
2024-09-07 13:51:19 +0900

    swagger - AuthHeaderParameter 추가

--- src/main/java/nameco/stikku/annotation/setting/UpdateSettingOperation.java
+++ src/main/java/nameco/stikku/annotation/setting/UpdateSettingOperation.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import nameco.stikku.annotation.AuthHeaderParameter;
 import nameco.stikku.annotation.UserIdParameter;
 
 import java.lang.annotation.ElementType;
@@ -16,6 +17,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Operation(summary = "사용자 설정 정보", description = "사용자 설정 정보를 조회합니다.")
 @UserIdParameter
+@AuthHeaderParameter
 @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "수정된 설정 정보", content = @Content(
                 mediaType = "application/json"
